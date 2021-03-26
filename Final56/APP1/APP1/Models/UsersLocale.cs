c2479d737b16371c551b84998d4355f3c67837d8
DahanAmir <80703040+DahanAmir@users.noreply.github.com>
2021-03-26 15:39:54 +0300

    עדכון אמיר

---
+++ Final56/APP1/APP1/Models/UsersLocale.cs
@@ -0,0 +1,43 @@
+﻿using APP1.Models.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace APP1.Models
+{
+    public class UsersDistrict
+    {
+        int id;
+        string email;
+        string district;
+
+        public UsersDistrict() { }
+
+        public UsersDistrict(int id, string email, string district)
+        {
+            Id = id;
+            Email = email;
+            District = district;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Email { get => email; set => email = value; }
+        public string District { get => district; set => district = value; }
+
+        public int Insert_arry_region(List<UsersDistrict> ud)
+        {
+            DB_Services db = new DB_Services();
+            return db.insert_arr_region(ud);
+        }
+
+
+        public List<UsersDistrict> get_User_district(string email)
+        {
+            DB_Services db = new DB_Services();
+            return db.get_User_district(email);
+        }
+
+    }
+    
+}
