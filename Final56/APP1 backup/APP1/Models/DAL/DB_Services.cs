03c20023071e1847eb0126cea2600c34b20a6731
DahanAmir <80703040+DahanAmir@users.noreply.github.com>
2021-04-05 11:44:37 +0300

    עדכון תצוגה

--- Final56/APP1 backup/APP1/Models/DAL/DB_Services.cs
+++ Final56/APP1 backup/APP1/Models/DAL/DB_Services.cs
@@ -60,20 +60,15 @@ namespace APP1.Models.DAL
             String command;
 
             StringBuilder sb = new StringBuilder();
-
             // use a string builder to create the dynamic string
             for (int i = 0; i < IUE.Count; i++)
             {
                 sb.AppendFormat("Values('{0}', '{1}', '{2}')", IUE[i].Email, IUE[i].UniversityName, IUE[i].Id);
             }
-
             String prefix = "INSERT INTO [UsersUniversity] " + "(Email,UniversityName, Id)";
             String delete = "DELETE FROM [UsersUniversity] WHERE Email=" + IUE[0].Email + " ";
             command = delete + " " + prefix + sb.ToString();
-
             return command;
-
-
 
         }
 
@@ -664,13 +659,8 @@ namespace APP1.Models.DAL
                     if (dr["Id"] == null)
                     {
                         ul.StatusId = Convert.ToInt32(dr["Id"]);
-                    }
-
-                
-
-                    
-
-                    list_of_user.Add(ul);
+                    }            
+                   list_of_user.Add(ul);
                 }
                 Users ule = new Users();
                 return list_of_user;
@@ -688,8 +678,6 @@ namespace APP1.Models.DAL
                     con.Close();
                 }
             }
-
-
         }
         public int Login_User(string email, string password)
         {
@@ -845,18 +833,13 @@ namespace APP1.Models.DAL
                 SqlDataReader dr = cmd.ExecuteReader(CommandBehavior.CloseConnection); // CommandBehavior.CloseConnection: the connection will be closed after reading has reached the end
 
                 while (dr.Read())
-                {
-                    
-
+                {              
                     uf.Email = (string)dr["Email"];
                     uf.UniversitySize = Convert.ToInt32(dr["UniversitySize"]);
                     uf.UniversityLevel = (string)dr["UniversityLevel"];
                     uf.PriceMAX = Convert.ToInt32(dr["PriceMAX"]);
                     uf.UniversityType = (string)dr["UniversityType"];
-                    uf.Sat = Convert.ToInt32(dr["Sat"]);
-
-
-                   
+                    uf.Sat = Convert.ToInt32(dr["Sat"]);         
                 }
                 return uf;
 
@@ -879,8 +862,6 @@ namespace APP1.Models.DAL
         //======================================================================
         //===============================function========================
         //======================================================================
-
-
         public List<University> getWish(string email)
         {
             List<University> wishlist = new List<University>();
@@ -922,17 +903,6 @@ namespace APP1.Models.DAL
 
 
         }
-
-
-
-
-
-
-
-
-
-
-
 
         public int SaveWishList(List <University> u)
         {
@@ -1007,17 +977,6 @@ namespace APP1.Models.DAL
 
             return command;
         }
-
-
-
-
-
-
-
-
-
-
-
 
 
         public SqlConnection connect(String conString)
